4b1f21fdeef6abda634f3a1f69a26fd74886a82a
chrisuzuki62 <chrisuzuki62@gmail.com>
2022-02-24 14:16:30 -0800

    Create mainpage.py

---
+++ mainpage.py
@@ -0,0 +1,34 @@
+'''@file                mainpage.py
+   @brief               Main page for documentation site.
+   @details             This is the main page for the doxygen site. It contains links to 
+                        the code for each component of ther term project some information about each one. 
+
+   @mainpage
+
+   @section sec_rep     Repository
+                        The repository for all the files can be found at 
+
+   @section sec_main    User main file
+
+   @section sec_read    Read G code file
+                        
+   @author              Cade Liberty
+   @author              Chris Suzuki
+
+   @date                November 24, 2022
+   @page    page1       Term Project Theory
+                        The Term Project's Finite State Machine and Task Diagrams
+                        \image html IMG_8314.jpg "Task Diagram"
+                        \image html IMG_8315.jpg "FSM2"
+                        \image html IMG_8316.jpg "FSM1"
+                        
+
+                        
+
+'''
+
+
+
+
+
+
