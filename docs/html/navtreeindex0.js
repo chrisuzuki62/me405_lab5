ad0c55ecca50c2aaf8be0837a19aa15dfc67931c
chrisuzuki62 <chrisuzuki62@gmail.com>
2022-02-24 23:42:06 -0800

    doxy3

---
+++ docs/html/navtreeindex0.js
@@ -0,0 +1,13 @@
+var NAVTREEINDEX0 =
+{
+"dir_68267d1309a1af8e8297ef4c3efbcdba.html":[2,0,0],
+"files.html":[2,0],
+"index.html":[],
+"index.html#sec_design":[1],
+"index.html#sec_plot":[1,2],
+"index.html#sec_rep":[0],
+"index.html#sec_task":[1,0],
+"index.html#sec_user":[1,1],
+"mainpage_8py.html":[2,0,0,0],
+"pages.html":[]
+};
